5f88e0eb39b8df190218223b224c9c200b996234
Artur <buch16@bk.ru>
2022-09-20 13:46:21 +0300

    Создана и заполнена блок-схема

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание. 
+//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых
+// меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на
+// старте выполнения алгоритма.
+
+Console.Clear();
+string[] stringArray = new string[3] { "1234", "1567", "-2"};
+string[] newArray = new string[stringArray.Length];
+int counter = 0;
+int size = 3;
+for (int a = 0; a < stringArray.Length; a++)
+{
+    if (stringArray[a].Length <= size)
+    {
+        newArray[counter] = stringArray[a];  
+        counter++;   
+    }
+}
+Console.Write($"[{String.Join(" ", stringArray)}] -> ");
+Console.Write($"[{String.Join(" ", newArray)}]");
